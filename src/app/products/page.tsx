79b623ee21378e124ddcab7e6a9d612757b61030
almirpask <almirpask@hotmail.com>
2024-02-03 12:42:15 -0300

    use ProductService

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -11,46 +11,13 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Link from "next/link";
 import Image from "next/legacy/image";
 import { ProductService } from "../../services/product.service";
-import { Product } from "../../models";
-
-async function getProducts({
-  search,
-  category_id,
-}: {
-  search?: string;
-  category_id?: string;
-}): Promise<Product[]> {
-  const urlSearchParams = new URLSearchParams();
-
-  if (search) {
-    urlSearchParams.append("search", search);
-  }
-
-  if (category_id) {
-    urlSearchParams.append("category_id", category_id);
-  }
-
-  let url = `${process.env.NEXT_API_URL}/products`;
-
-  if (urlSearchParams.toString()) {
-    url += `?${urlSearchParams.toString()}`;
-  }
-
-  const response = await fetch(url, {
-    next: {
-      revalidate: 1,
-    },
-  });
-
-  return response.json();
-}
 
 async function ListProductsPage({
   searchParams,
 }: {
   searchParams: { search?: string; category_id?: string };
 }) {
-  const products = await getProducts({
+  const products = await new ProductService().getProducts({
     search: searchParams.search,
     category_id: searchParams.category_id,
   });
