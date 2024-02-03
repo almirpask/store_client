2e97e6efef11a776a2a44543057085c4d1d01a68
almirpask <almirpask@hotmail.com>
2024-02-03 12:30:46 -0300

    add redirect route

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/products",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
